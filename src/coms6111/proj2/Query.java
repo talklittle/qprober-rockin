e68c6893a8699973802b584ff036fba4539fa0f5
talklittle <talklittle@6804bb76-1102-11de-9cb0-39e535855b64>
2009-03-23 00:01:43 +0000

    [Query] Add Query class from proj1 (modified)

---
+++ src/coms6111/proj2/Query.java
@@ -0,0 +1,108 @@
+package coms6111.proj2;
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.index.Term;
+
+public class Query {
+	private static Log log = LogFactory.getLog(Query.class);
+	
+	public final String appid = "SeJQZ5fV34F7ohb4ONiSH9bbdWH9RtbodjvH_cN_BRj9QWEgfSFLW1h.Jkj0i52LT6I-";
+	
+	private String myQueryString;
+	private List<String> myQueryStringList; // query string broken into individual terms
+	
+	public Query(String queryString) {
+		this.setString(queryString);
+	}
+	public Query(Term[] termsInOrder) {
+		myQueryString = "";
+		for (Term t : termsInOrder) {
+			myQueryString += (t.text() + " ");
+		}
+		myQueryString = myQueryString.trim();
+		this.setString(myQueryString);
+	}
+	
+	public Resultset execute(int numResults) {
+		String request = "http://api.search.yahoo.com/WebSearchService/V1/webSearch";
+	    HttpClient client = new HttpClient();
+
+	    PostMethod method = new PostMethod(request);
+
+	    // Add POST parameters
+	    method.addParameter("appid", appid);
+	    method.addParameter("query", myQueryString);
+	    method.addParameter("results", ""+numResults);
+
+	    // Send POST request
+	    try {
+	    	int statusCode = client.executeMethod(method);
+	        if (statusCode != HttpStatus.SC_OK) {
+	        	log.error("Method failed: " + method.getStatusLine());
+	        	return null;
+	        }
+	    } catch (IOException e) {
+	    	log.error(e.getLocalizedMessage());
+	    	return null;
+	    }
+
+	    InputStream rstream = null;
+	    
+	    // Get the response body
+	    try {
+	    	rstream = method.getResponseBodyAsStream();
+	    } catch (IOException e) {
+	    	log.error(e.getLocalizedMessage());
+	    	return null;
+	    }
+
+	    try {
+	    	return new Resultset(rstream);
+	    } catch (Exception e) {
+	    	log.warn("Error creating Resultset from result stream");
+	    	return null;
+	    }
+	}
+
+	/**
+	 * Return the queryString enclosed by square brackets.
+	 */
+	public String toString() {
+		return "[" + myQueryString + "]";
+	}
+	
+	/**
+	 * Set the string of the query.
+	 * @param queryString The String of the Query
+	 */
+	public void setString(String queryString) {
+		StringTokenizer st = new StringTokenizer(queryString);
+		myQueryStringList = new ArrayList<String>();
+		while (st.hasMoreTokens())
+			myQueryStringList.add(st.nextToken());
+		myQueryString = queryString;
+	}
+	
+	/**
+	 * Return an Iterator<String> to inspect each query term separately.
+	 * @return Iterator<String>
+	 */
+	public Iterator<String> iterator() {
+		return myQueryStringList.iterator();
+	}
+	
+	/**
+	 * Return the number of terms in the query
+	 * @return int of number of terms in the query
+	 */
+	public int length() {
+		return myQueryStringList.size();
+	}
+}
