ac5d2c22d207179b261bd341d0b277c1bb0daa84
talklittle <talklittle@6804bb76-1102-11de-9cb0-39e535855b64>
2009-03-23 04:26:02 +0000

    [ContentSummary] Create content summaries
    Part 2b

--- src/coms6111/proj2/ContentSummary.java
+++ src/coms6111/proj2/ContentSummary.java
@@ -2,6 +2,7 @@ package coms6111.proj2;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -11,35 +12,38 @@ public class ContentSummary {
 	
 
 	private String databaseName;
-	private HashMap<String, Double> docFreq;
+	private Map<String, Integer> docFreqs;
 	
 	public ContentSummary(String newDatabaseName) {
 		databaseName = newDatabaseName;
-		docFreq = new HashMap<String, Double>();
+		docFreqs = new HashMap<String, Integer>();
 	}
 	
-	public void setDocFreq(String word, Double freq) {
-		docFreq.put(word, freq);
+	public void setDocFreq(String word, Integer freq) {
+		docFreqs.put(word, freq);
 	}
 	
-	public void setDocFreqs(List<String> words, List<Double> freqs) {
+	public void setDocFreqs(Map<String, Integer> newDocFreqs) {
+		docFreqs = newDocFreqs;
+	}
+	
+	public void setDocFreqs(List<String> words, List<Integer> freqs) {
 		if (words.size() != freqs.size()) {
 			log.warn("ContentSummary: setDocFreqs: words.size() != freqs.size() ("
 					+ words.size() + ", " + freqs.size() + ")");
 			return;
 		}
 		for (int i = 0; i < words.size(); i++) {
-			docFreq.put(words.get(i), freqs.get(i));
+			docFreqs.put(words.get(i), freqs.get(i));
 		}
 	}
 	
-	public Double getDocFreq(String word) {
-		return docFreq.get(word);
+	public Integer getDocFreq(String word) {
+		return docFreqs.get(word);
 	}
 	
-	@SuppressWarnings("unchecked")
-	public HashMap<String, Double> getDocFreqs() {
-		return (HashMap<String, Double>) docFreq.clone();
+	public Map<String, Integer> getDocFreqs() {
+		return docFreqs;
 	}
 	
 	public String getDatabaseName() {
--- src/coms6111/proj2/ContentSummaryConstructor.java
+++ src/coms6111/proj2/ContentSummaryConstructor.java
@@ -1,18 +1,56 @@
 package coms6111.proj2;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class ContentSummaryConstructor {
 	private static Log log = LogFactory.getLog(ContentSummaryConstructor.class);
 	
+	private Resultset contentResultset;
 	
 	// TODO
-	public ContentSummaryConstructor() {
-		
+	public ContentSummaryConstructor(Resultset resultset) {
+		contentResultset = resultset;
 	}
 	
+	public ContentSummary construct(String databaseName) {
+		ContentSummary returnMe;
+		HashMap<String, Integer> docFreqs = new HashMap<String, Integer>();
+		
+		if (contentResultset == null) {
+			log.warn("construct() called with null contentResultset");
+			return null;
+		}
+		
+		returnMe = new ContentSummary(databaseName);
+		
+		for (Iterator<Result> it = contentResultset.iterator(); it.hasNext(); ) {
+			Result r = it.next();
+			Set<String> words = LynxRunner.runLynx(r.url.toString());
+			for (String word : words) {
+				if (docFreqs.containsKey(word)) {
+					docFreqs.put(word, docFreqs.get(word)+1);
+				} else {
+					docFreqs.put(word, 1);
+				}
+			}
+		}
+		
+		returnMe.setDocFreqs(docFreqs);
+		return returnMe;
+	}
 	
+	public Resultset getResultset() {
+		return contentResultset;
+	}
+	
+	public void setResultset(Resultset newResultset) {
+		contentResultset = newResultset;
+	}
 	
 	
 }
