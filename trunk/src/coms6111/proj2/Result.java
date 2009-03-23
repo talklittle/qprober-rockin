ad06517c3fa73f18811a93334597c3e8601ea32f
talklittle <talklittle@6804bb76-1102-11de-9cb0-39e535855b64>
2009-03-23 00:01:43 +0000

    [Query] Add Query class from proj1 (modified)

---
+++ trunk/src/coms6111/proj2/Result.java
@@ -0,0 +1,36 @@
+package coms6111.proj2;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Represents one result as returned by search engine
+ */
+public class Result {
+	private static Log log = LogFactory.getLog(Result.class);
+	
+	public String title, summary;
+	public URL url;
+	
+	/**
+	 * Constructor.
+	 * @param theTitle Title of a search engine result.
+	 * @param theSummary Summary text of a search engine result.
+	 * @param theUrl URL returned by search engine for this result.
+	 */
+	public Result(String theTitle, String theSummary, String theUrl) {
+		title = theTitle;
+		summary = theSummary;
+		try {
+			url = new URL(theUrl);
+		} catch (MalformedURLException e) {
+			log.warn(e);
+			url = null;
+		}
+	}
+	
+	
+}
---
+++ trunk/src/coms6111/proj2/Resultset.java
@@ -0,0 +1,86 @@
+package coms6111.proj2;
+
+import java.io.*;
+import java.util.*;
+
+import javax.xml.xpath.*;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.*;
+
+/**
+ * A Resultset either from search engine or manually created.
+ */
+public class Resultset {
+	private static Log log = LogFactory.getLog(Resultset.class);
+	
+	private List<Result> myResults;
+	private int resultSize;
+	
+	public Resultset(List<Result> results) {
+		myResults = results;
+		if (results != null)
+			resultSize = results.size();
+		else
+			resultSize = 0;
+	}
+	
+	/**
+	 * Constructor using input stream returned from HttpClient
+	 */
+	public Resultset(InputStream rstream) throws Exception {
+		Document response = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(rstream);
+		
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xPath=factory.newXPath();
+
+		//Get all search Result nodes
+		NodeList nodes = (NodeList)xPath.evaluate("/ResultSet/Result", response, XPathConstants.NODESET);
+		int nodeCount = nodes.getLength();
+        
+		myResults = new ArrayList<Result>();
+		resultSize = 0;
+		//iterate over search Result nodes
+		for (int i = 0; i < nodeCount; i++) {
+			//Get each xpath expression as a string
+			String title = (String)xPath.evaluate("Title", nodes.item(i), XPathConstants.STRING);
+			String summary = (String)xPath.evaluate("Summary", nodes.item(i), XPathConstants.STRING);
+			String url = (String)xPath.evaluate("Url", nodes.item(i), XPathConstants.STRING);
+			//print out the Title, Summary, and URL for each search result
+			log.info("-- Result " + (i+1));
+			log.info("Title: " + title);
+			log.info("Summary: " + summary);
+			log.info("URL: " + url);
+			log.info("--");
+			
+			myResults.add(new Result(title, summary, url));
+			resultSize++;
+		}
+	}
+	
+	/**
+	 * Get an in-order Iterator for the Resultset.
+	 * @return An in-order Iterator for the Resultset.
+	 */
+	public Iterator<Result> iterator() {
+		return myResults.iterator();
+	}
+	/**
+	 * Get an in-order ListIterator for the Resultset.
+	 * @return An in-order ListIterator for the Resultset.
+	 */
+	public ListIterator<Result> listIterator() {
+		return myResults.listIterator();
+	}
+	
+	/**
+	 * Return the number of results.
+	 * @return The number of results contained in this Resultset.
+	 */
+	public int getSize() {
+		return resultSize;
+	}
+}
