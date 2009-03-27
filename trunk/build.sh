478802d0452c997686db6675385ad11ec9c1a292
talklittle <talklittle@6804bb76-1102-11de-9cb0-39e535855b64>
2009-03-27 02:41:55 +0000

    [cleanup]

--- trunk/build.sh
+++ trunk/build.sh
@@ -15,3 +15,4 @@ fi
 javac -classpath "$CLASSPATH:$LIBDIR/junit-4.5.jar:$LIBDIR/commons-codec-1.3/commons-codec-1.3.jar:$LIBDIR/commons-httpclient-3.1/commons-httpclient-3.1.jar:$LIBDIR/commons-logging-1.1.1/commons-logging-1.1.1.jar:$LIBDIR/commons-lang-2.4/commons-lang-2.4.jar:$LIBDIR/apache-log4j-1.2.15/log4j-1.2.15.jar:$PWD/src" -d "$OUTDIR" $PWD/src/coms6111/proj2/*.java
 
 cp src/log4j.properties bin/
+cp src/defaultClassifications.xml bin/
--- trunk/maketar.sh
+++ trunk/maketar.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-mkdir ans2120-proj1
+mkdir ans2120-proj2
 if [ $? != 0 ]; then
     exit 1
 fi
 
-cp -R src Readme.pdf transcript.txt build.sh run.sh maketar.sh \
-    Makefile ans2120-proj1
+cp -R src Readme.pdf build.sh run.sh maketar.sh Makefile ans2120-proj2
 
-tar czvf ans2120-proj1.tar.gz ans2120-proj1
+tar czvf ans2120-proj2.tar.gz ans2120-proj2
 
-rm -rf ans2120-proj1
+rm -rf ans2120-proj2
