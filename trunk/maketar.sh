58a5ff3b0ddad5d20e04d672a6a615605cbd2c1f
talklittle <talklittle@6804bb76-1102-11de-9cb0-39e535855b64>
2009-03-27 03:24:52 +0000

    [readme]

--- trunk/maketar.sh
+++ trunk/maketar.sh
@@ -5,7 +5,8 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-cp -R src Readme.pdf build.sh run.sh maketar.sh Makefile ans2120-proj2
+cp -R src README.txt Readme.pdf build.sh run.sh maketar.sh Makefile \
+      ans2120-proj2
 
 tar czvf ans2120-proj2.tar.gz ans2120-proj2
 
